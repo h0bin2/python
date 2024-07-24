24439f04da8da432a7d0ff4cbe2abd8e11214477
ttpo3270 <ttpo3270@gmail.com>
2024-07-24 10:57:45 +0000

    update youtube crawler

--- youtube.py
+++ youtube.py
@@ -1,6 +1,32 @@
 import pytchat
+import datetime
+import pandas as pd
 
-chat = pytchat.create(video_id="vOMZtrbeSm8")
-while chat.is_alive():
-    for c in chat.get().sync_items():
-        print(f"{c.datetime} [{c.author.name}]- {c.message}")
+class Youtube():
+    def __init__(self, url):
+        self.url = url
+        self.id = self.__getID__()
+
+    def __getID__(self):
+        id = self.url.split("?")[-1].split("=")[-1]
+        return id
+
+class Chat(Youtube):
+    def __init__(self, url):
+        super().__init__(url)
+        self.chat = pytchat.create(video_id=self.id)
+
+    def connect(self):
+        while self.chat.is_alive():
+            for c in self.chat.get().sync_items():
+                print(f"{c.datetime} [{c.author.name}]- {c.message}")
+
+live = "https://www.youtube.com/watch?v=vOMZtrbeSm8"
+yt = Chat(live)
+yt.connect()
+
+#chat = pytchat.create(video_id="vOMZtrbeSm8")
+
+# while chat.is_alive():
+#     for c in chat.get().sync_items():
+#         print(f"{c.datetime} [{c.author.name}]- {c.message}")
